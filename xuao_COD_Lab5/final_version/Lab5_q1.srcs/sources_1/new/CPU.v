6dd545a2729fd63cfa894a20e788eef7f1241e52
JerryKwan <ggxx936099147@mail.ustc.edu.cn>
2022-05-12 20:21:24 +0800

    MEM_B

--- xuao_COD_Lab5/final_version/Lab5_q1.srcs/sources_1/new/CPU.v
+++ xuao_COD_Lab5/final_version/Lab5_q1.srcs/sources_1/new/CPU.v
@@ -243,7 +243,7 @@ always @(posedge clk or negedge rstn) begin
     end    
     else begin
         Mem_ALU_result <= ALU_result;
-        Mem_B <= EX_B;
+        Men_B <= ((Mem_IR[6:0]==7'b0010011 || Mem_IR[6:0]==7'b0110011 || Mem_IR[6:0]==7'b0010111 || Mem_IR[6:0]==7'b0000011) && EX_IR[24:20]==Mem_IR[11:7]) ? Mem_ALU_result : (((WB_IR[6:0]==7'b0010011 || WB_IR[6:0]==7'b0110011 || WB_IR[6:0]==7'b0010111 || WB_IR[6:0]==7'b0000011) && EX_IR[24:20]==WB_IR[11:7]) ? WB_ALU_result : EX_B);
         Mem_ctrl <= {EX_ctrl[31:10],ALU_lessthan,ALU_equal,EX_ctrl[7:6],PCSrc,EX_ctrl[4:3],MemWrite,EX_ctrl[1:0]};
         Mem_IR <= EX_IR;
         Mem_pc <= EX_pc;
